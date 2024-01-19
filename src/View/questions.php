296eb50f938afa60e3e340d2680f18e042c50cc6
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-01-19 11:44:45 +0100

    affichage des enigmes

--- src/View/questions.php
+++ src/View/questions.php
@@ -3,6 +3,15 @@
 <?php ob_start(); ?>
 <h1>Les questions de nos utilisateurs</h1>
 
+<div class="container">
+        <?php foreach ($enigmes as $enigme) { ?>
+    <div class="card">
+            <h3><?= $enigme['question']; ?></h3>
+            <button>Voir l'énigme</button>
+    </div>
+        <?php } ?>
+</div>
+
 <?php $content = ob_get_clean(); ?>
 
 
