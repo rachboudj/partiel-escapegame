3de07f67ca5fbd9705941aa05b03e9ad718feca6
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-01-19 11:11:04 +0100

    ajout du formulaire + message de succès

--- src/View/accueil.php
+++ src/View/accueil.php
@@ -3,7 +3,29 @@
 <?php ob_start(); ?>
 <h1>Posez votre question !</h1>
 
-<?php $content = ob_get_clean(); ?>
+
+<form method="POST" action="index.php?page=accueil">
+    <label for="question">Question</label>
+    <input type="text" name="question" />
+    <span class="error"><?php if (!empty($errors['question'])) {
+                            echo $errors['question'];
+                        } ?></span>
+
+    <label for="reponse">Réponse</label>
+    <input type="reponse" name="reponse" />
+    <span class="error"><?php if (!empty($errors['reponse'])) {
+                            echo $errors['reponse'];
+                        } ?></span>
+
+    <input class="buttons" type="submit" name="submit" value="Valider mon énigme" />
+</form>
 
 
-<?php require('./src/View/layout.php'); ?>
+<?php if (!empty($success)) { ?>
+    <p class="success-message"><?php echo $success; ?></p>
+
+<?php }
+
+$content = ob_get_clean(); 
+
+require('./src/View/layout.php'); ?>
