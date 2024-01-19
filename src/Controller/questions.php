3eca149e1799053fc48ee4070e41ce512255b9db
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-01-19 11:44:14 +0100

    envoie des données vers la vue

--- src/Controller/questions.php
+++ src/Controller/questions.php
@@ -1,6 +1,14 @@
 <?php 
 
+require_once './src/Model/enigme.php';
+
 
 function questions() {
+    $pdo = (new Database())->getPdo();
+    $model = new Enigme($pdo);
+
+    $enigmes = $model->showEnigme();
+
+
     require('./src/View/questions.php');
 }
